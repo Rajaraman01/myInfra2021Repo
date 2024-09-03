acc1749732cbccd135e8d02e4f7f5005bf57f0b7
Rajaraman01 <rajaraman.j@yahoo.com>
2024-09-03 19:36:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "ap-southeast-2" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "myEC2Key" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-0892a9c01908fafd1" 
 }
 variable "versioning" {
     type        = bool
